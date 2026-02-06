8ae06a787413b9172b82af1cc4fadfed438118ab
sodacan^v^ <reinwaldmarone.valeza.320401@gmail.com>
2026-02-06 19:59:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 import streamlit as st
 import pandas as pd
 
-row_labels = ["Task Name", "Priority Value", "Duration"]
+st.markdown("<h1>Hey Mickey</h1>");
 
+row_labels = ["Task Name", "Priority Value", "Duration"]
 data = ["Cherry Pie Pikachu", "69", "1 hrs"]
 
 df = pd.DataFrame(data, index = row_labels, columns=[""])
 
 with st.expander("Task: Clean the Room", expanded=False):
     st.table(df)
+
 
 
 

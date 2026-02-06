73fa46d3baf30188286154866dde412916ba27b9
sodacan^v^ <reinwaldmarone.valeza.320401@gmail.com>
2026-02-06 20:05:30 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 import streamlit as st
 import pandas as pd
 
-def task (row, column):
+def task (row, column, name):
     df = pd.DataFrame(column, index = row, columns=[""])
-    with st.expander("Task: Clean the Room", expanded=False):
+    with st.expander(name, expanded=False):
         st.table(df)
-
-
-st.markdown("<h1>Hey Mickey</h1>", unsafe_allow_html=True);
 
 row = ["Task Name", "Priority Value", "Duration"]
 column = ["Cherry Pie Pikachu", "69", "1 hrs"]
 
-task(row, column)
+st.markdown("<h2>Monday</h2>", unsafe_allow_html=True);
+st.markdown("<h3>Low Priority</h3>", unsafe_allow_html=True);
+task(row, column, "Poodle-rama")
+
 
 
 

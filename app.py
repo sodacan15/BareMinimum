918f43ec654d619f886cd86eeebec13c9a5e1cac
sodacan^v^ <reinwaldmarone.valeza.320401@gmail.com>
2026-02-06 19:53:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ row_labels = ["Task Name", "Priority Value", "Duration"]
 
 data = ["Cherry Pie Pikachu", "69", "1 hrs"]
 
-df = pd.DataFrame(data, index = row_labels)
+df = pd.DataFrame(data, index = row_labels, columns=[""])
 
 with st.expander("Task: Clean the Room", expanded=False):
     st.table(df)
+
 
 
 

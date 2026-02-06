dafac6a7986cb0d0f86246acd2592e6bfc283135
sodacan^v^ <reinwaldmarone.valeza.320401@gmail.com>
2026-02-06 19:45:23 +0800

    0.1

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-row_labels= = ["A", "B", "C"]
+row_labels = ["A", "B", "C"]
 
 data = {
     "Task Name": ["Math Assignment", "Gym", "Laundry"],
@@ -13,6 +13,7 @@ df = pd.DataFrame(data, index = row_labels)
 
 with st.expander("Task: Clean the Room", expanded=False):
     st.table(df)
+
 
 
 

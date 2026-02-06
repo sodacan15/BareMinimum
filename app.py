5d736f445a8d51710d04629f908d45cf14101e2c
sodacan^v^ <reinwaldmarone.valeza.320401@gmail.com>
2026-02-06 19:46:56 +0800

    Fix data structure in DataFrame creation

--- app.py
+++ app.py
@@ -4,15 +4,16 @@ import pandas as pd
 row_labels = ["A", "B", "C"]
 
 data = {
-    "Task Name": ["Math Assignment"],
-    "Priority": [85],
-    "Duration": ["1h 30m"]
+     ["Math Assignment"],
+    [85],
+    ["1h 30m"]
 }
 
 df = pd.DataFrame(data, index = row_labels)
 
 with st.expander("Task: Clean the Room", expanded=False):
     st.table(df)
+
 
 
 
